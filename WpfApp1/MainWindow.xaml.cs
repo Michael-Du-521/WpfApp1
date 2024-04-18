087875681fa7b3aa4f0efb5f252688878377ef54
Miguel <Miguel@DESKTOP-BRVQ87D>
2024-04-19 00:56:03 +0800

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (null!=sender)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            else
+            {
+                MessageBox.Show($"{sender?.ToString()} could be null in any sense", "Warning");
+            }
+            
+        }
+    }
+}
