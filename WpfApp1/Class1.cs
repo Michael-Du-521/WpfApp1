05f99a42654e28838bc86a872b4cc22f6633b980
Miguel_Du <13104391508@163.com>
2024-04-21 01:27:09 +0800

    1 Commit All test

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    internal class Class1
+    {
+        int stationId;
+        int dataBits;
+        private int stopBit;
+
+        int StopBit { get => stopBit; set => stopBit = value; }
+        static bool isRunning;
+
+        public static bool Start() { 
+        if(true == isRunning) {
+
+                return false;
+            }
+            try
+            {
+                isRunning = true;
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return true;
+           
+
+        
+        }
+    }
+}
